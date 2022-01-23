47f58a2df6bb92aed781a10df81867569afc9f8e
Mary <mary.nazaretian@gmail.com>
2022-01-23 12:16:52 +0400

    GitHub User Analyzer: Configurable filters added.

--- src/main/java/com/disqo/mary/gitanalyzer/config/AnalyzerConfig.java
+++ src/main/java/com/disqo/mary/gitanalyzer/config/AnalyzerConfig.java
@@ -12,11 +12,17 @@ import org.springframework.context.annotation.Configuration;
 @ConfigurationProperties(prefix = "disqo.mary.git.analyzer")
 public class AnalyzerConfig implements InitializingBean {
     private int restriction;
+
     private String url;
     private String userUrl;
+
     private String token;
     private String user;
     private String password;
+
+    private int repos;
+    private int followers;
+    private String createdAfter;// TODO: try to bind Date from properties.
 
     @Override
     public void afterPropertiesSet() {
--- src/main/java/com/disqo/mary/gitanalyzer/job/GitHubUserWriter.java
+++ src/main/java/com/disqo/mary/gitanalyzer/job/GitHubUserWriter.java
@@ -1,7 +1,12 @@
 package com.disqo.mary.gitanalyzer.job;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.function.Predicate;
 
+import com.disqo.mary.gitanalyzer.config.AnalyzerConfig;
 import com.disqo.mary.gitanalyzer.model.entity.UserDetails;
 import com.disqo.mary.gitanalyzer.service.GitHubUserService;
 import lombok.RequiredArgsConstructor;
@@ -13,9 +18,28 @@ import org.springframework.stereotype.Component;
 public class GitHubUserWriter implements ItemWriter<UserDetails> {
 
     private final GitHubUserService gitHubUserService;
+    private final AnalyzerConfig config;
+
+    private final static SimpleDateFormat FORMATTER = new SimpleDateFormat("yyyy-MM-dd");
 
     @Override
-    public void write(List<? extends UserDetails> items) {
-        items.forEach(gitHubUserService::save);
+    public void write(List<? extends UserDetails> items) throws ParseException {
+        items.stream()
+            .filter(reposMoreThan(config.getRepos()))
+            .filter(followersMoreThan(config.getRepos()))
+            .filter(profileCreatedAfter(FORMATTER.parse(config.getCreatedAfter())))
+            .forEach(gitHubUserService::save);
+    }
+
+    private Predicate<UserDetails> reposMoreThan(int repos) {
+        return userDetails -> userDetails.getPublicRepos() > repos;
+    }
+
+    private Predicate<UserDetails> followersMoreThan(int followers) {
+        return userDetails -> userDetails.getFollowers() > followers;
+    }
+
+    private Predicate<UserDetails> profileCreatedAfter(Date date) {
+        return userDetails -> userDetails.getCreatedAt().after(date);
     }
 }
